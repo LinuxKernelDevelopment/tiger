e3346fca9eb0efa0dc70377e7fb90f88ff7857cf
Flint.Wang <hmsjwzb@zoho.com>
2022-08-02 03:02:22 -0400

    Change semant.ml

--- chap05/semant.ml
+++ chap05/semant.ml
@@ -74,3 +74,101 @@ let rec trans_var venv tenv break level = function
               Error_msg.error pos (Error_msg.Type_mismatch ("record", T.string_of_ty ty));
               {exp = Translate.unit_exp; ty = T.INT}
         end
+  | A.SubscriptVar (var, i, pos) ->
+     let {exp = array_exp; ty = array_ty} = trans_var venv tenv break level var in
+       begin
+         match actual_ty array_ty with
+         | T.ARRAY (elem_ty, _) ->
+             let {exp = subscript_exp; ty = subscript_ty} as subscript_expty = trans_exp venv tenv break level i in
+               checkint subscript_expty pos;
+               let exp = Translate.subscript_var array_exp subscript_exp in
+               let ty = actual_ty elem_ty in
+                 {exp; ty}
+         | ty ->
+             Error_msg.error pos (Error_msg.Type_mismatch ("array", T.string_of_ty ty));
+             {exp = Translate.unit_exp; ty = T.INT}
+       end
+
+and trans_exp venv tenv break level exp =
+  let rec trexp = function
+    | A.VarExp var ->
+        trans_var venv tenv break level var
+    | A.NilExp ->
+        {exp = Translate.nil_exp; ty = T.NIL}
+    | A.IntExp i ->
+        {exp = Translate.int_exp i; ty = T.INT}
+    | A.StringExp (str, _) ->
+        {exp = Translate.string_exp str; ty = T.STRING}
+    | A.CallExp (name, args, pos) ->
+        begin
+          match S.look name venv with
+          | Some (Env.FunEntry (flevel, label, formal_tys, result_ty)) ->
+             let numformals = List.length formal_tys in
+             let numactuals = List.length args in
+               if numformals <> numactuals then
+                 begin
+                   Error_msg.error pos (Error_msg.Arity_mismatch (numformals, numactuals));
+                   {exp = Translate.unit_exp; ty = T.INT}
+                 end
+               else
+                 let arg_exps = List.map2
+                     (fun formal_ty arg ->
+                        let {exp; _ } as expty = trexp arg in check_expty formal_ty expty pos; exp)                     formal_tys
+                     args in
+                    {exp = Translate.call_exp flevel level label arg_exps; ty = result_ty}
+          | _ ->
+              Error_msg.error pos (Error_msg.Undefined_function (S.name name));
+              {exp = Translate.unit_exp; ty = T.INT}
+         end
+    | A.OpExp (l, op, r, pos) when op = A.EqOp || op = A.NeqOp ->
+        let {exp = lexp; ty = lty} = trexp l in
+        let {exp = rexp; ty = rty} = trexp r in
+          checkcomp lty rty pos;
+          let exp = match op, lty, rty with
+              A.EqOp, T.STRING, T.STRING -> Translate.string_eq_op_exp lexp rexp
+            | A.NeqOp, T.STRING, T.STRING -> Translate.string_ne_op_exp lexp rexp
+            | A.Eqop, _, _ -> Translate.eq_op_exp lexp rexp
+            | A.NeqOp, _, _ -> Translate.ne_op_exp lexp rexp
+            | _ -> raise (Failure "trans_exp")
+          in {exp; ty = T.INT}
+    | A.OpExp (l, op, r, pos) ->
+        let {exp = lexp; _} as lexpty = trexp l in
+        let {exp = rexp; _} as rexpty = trexp r in
+        checkint lexpty pos;
+        checkint rexpty pos;
+        let exp = match op with
+            A.PlusOp -> Translate.plus_op_exp lexp rexp
+          | A.MinusOp -> Translate.minus_op_exp lexp rexp
+          | A.TimesOp -> Translate.mul_op_exp lexp rexp
+          | A.DivideOp -> Translate.div_op_exp lexp rexp
+          | A.LtOp -> Translate.lt_op_exp lexp rexp
+          | A.LeOp -> Translate.le_op_exp lexp rexp
+          | A.GtOp -> Translate.gt_op_exp lexp rexp
+          | A.GeOp -> Translate.ge_op_exp lexp rexp
+          | _ -> raise (Failure "trans_exp")
+        in {exp; ty = T.INT}
+    | A.RecordExp (fields, ty_id, pos) ->
+        begin
+          match S.look ty_id tenv with
+          | Some record_ty ->
+              begin
+                match actual_ty record_ty with
+                | T.RECORD (field_tys, _) as record_ty' ->
+                    let field_exps = List.map2
+                        (fun (name, ty) (name', exp, pos) ->
+                           if name <> name' then
+                             Error_msg.error pos (Error_msg.Record_field_mismatch (S.name name, S.name name'));
+                           let {exp = field_exp; ty = field_ty} as field_expty = trexp exp in
+                             check_expty (actual_ty ty) field_expty pos;
+                             field_exp)
+                        field_tys
+                        fields
+                    in {exp = Translate.record_exp field_exps; ty = record_ty'}
+                | ty ->
+                    Error_msg.error pos (Error_msg.Type_mismatch ("record", T.string_of_ty ty));
+                    {exp = Translate.unit_exp; ty = T.RECORD ([], ref ())}
+              end
+          | None ->
+              Error_msg.error pos (Error_msg.Undefined_record (S.name ty_id));
+              {exp = Translate.unit_exp; ty = T.RECORD([], ref ())}
+        end
